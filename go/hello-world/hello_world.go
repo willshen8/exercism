de6e53277980af68f7380eb0cc4fa2ad6f42ede1
Will Shen <will.shen@anz.com>
2019-09-20 10:29:44 +1000

    Started Exercism GO stream by completing the classic hello-world exercise

---
+++ go/hello-world/hello_world.go
@@ -0,0 +1,7 @@
+// Package greeting simply returns a string with hello world
+package greeting
+
+// HelloWorld simply returns a string with 'Hello World!'
+func HelloWorld() string {
+	return "Hello, World!"
+}
