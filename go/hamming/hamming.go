6f59138dcef365f96c1860d48fc7de3c73066236
Will Shen <will.shen@anz.com>
2019-09-24 10:57:17 +1000

    Completing exercise on hamming distance

---
+++ go/hamming/hamming.go
@@ -0,0 +1,22 @@
+package hamming
+
+import (
+	"errors"
+)
+
+// Distance calculates the hamming distance between two strings/DNA strands
+func Distance(a, b string) (int, error) {
+	if a == "" && b == "" {
+		return 0, nil
+	}
+	if len(a) != len(b) {
+		return 0, errors.New("Two strings have different length")
+	}
+	var distance int
+	for i := 0; i < len(a); i++ {
+		if a[i] != b[i] {
+			distance++
+		}
+	}
+	return distance, nil
+}
