36a645eedc73d29a51f92c23a4aab21c1f989a08
Will Shen <will.shen@anz.com>
2019-10-19 21:21:45 +1100

    Finish scrabble exercise

---
+++ go/scrabble-score/scrabble_score.go
@@ -0,0 +1,48 @@
+package scrabble
+
+import (
+	"strings"
+)
+
+var scoreMap = map[string]int{
+	"A": 1,
+	"E": 1,
+	"I": 1,
+	"O": 1,
+	"U": 1,
+	"L": 1,
+	"N": 1,
+	"R": 1,
+	"S": 1,
+	"T": 1,
+	"D": 2,
+	"G": 2,
+	"B": 3,
+	"C": 3,
+	"M": 3,
+	"P": 3,
+	"F": 4,
+	"H": 4,
+	"V": 4,
+	"W": 4,
+	"Y": 4,
+	"K": 5,
+	"J": 8,
+	"X": 8,
+	"Q": 10,
+	"Z": 10,
+}
+
+func Score(s string) int {
+
+	var result int
+	for _, c := range s {
+		searchChar := strings.ToUpper(string(c))
+		searchString := string(searchChar)
+		if score, found := scoreMap[searchString]; found {
+			result += score
+		}
+
+	}
+	return result
+}
