0d99d60b03b62f112928257659600114ee7b683d
Will Shen <will.shen@anz.com>
2019-12-23 16:32:53 +1100

    made to pass all tests, especially compare functions

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -6,46 +6,48 @@ import (
 
 // Clock represent a real clock with minute and hour
 type Clock struct {
-	hour   int
 	minute int
 }
 
 func (c Clock) String() string {
-	return fmt.Sprintf("%02d:%02d", c.hour, c.minute)
+	return fmt.Sprintf("%02d:%02d", c.minute/60, c.minute%60)
 }
 
 // New function create a clock with the minute and hour passed in
 func New(h, m int) Clock {
-	return Clock{hour: h, minute: m}.FormatClock()
+	return Clock{minute: NormaliseClock(m + h*60)}
 }
 
 // Add will plus a certain minutes to a clock
 func (c Clock) Add(minutes int) Clock {
-	c.minute += minutes
-	return c.FormatClock()
+	c.minute = NormaliseClock(c.minute + minutes)
+	return c
 }
 
 // Subtract take away x minutes from a clock
 func (c Clock) Subtract(minutes int) Clock {
-	c.minute-= minutes
-	return c.FormatClock()
+	c.minute = NormaliseClock(c.minute - minutes)
+	return c
 }
 
-// FormatClock format a clock into the correct format
-func (c Clock) FormatClock() Clock {
-	if c.minute < 0 {
-		c.hour = c.hour - 1 - (-c.minute)/60
-		c.minute = 60 - (-c.minute)%60
+// NormaliseClock normalise a clock in the right format
+func NormaliseClock(m int) int {
+	hour := m / 60
+	minute := m % 60
+
+	if minute < 0 {
+		hour = hour - 1 - (-minute)/60
+		minute = 60 - (-minute)%60
 	}
-	if c.hour < 0 {
-		c.hour = 24 - (-c.hour)%24
+	if hour < 0 {
+		hour = 24 - (-hour)%24
 	}
-	if c.minute >= 60 {
-		c.hour = c.hour + c.minute/60
-		c.minute = c.minute % 60
+	if minute >= 60 {
+		hour = hour + m/60
+		minute = minute % 60
 	}
-	if c.hour >= 24 {
-		c.hour = c.hour % 24
+	if hour >= 24 {
+		hour = hour % 24
 	}
-	return c
+	return hour*60 + minute
 }
