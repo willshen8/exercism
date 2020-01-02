617f351fbc5c67215a1aa91a6246a19f7cb498f1
Will Shen <will.shen@anz.com>
2020-01-02 21:58:08 +1100

    simplified clock function

--- go/clock/clock.go
+++ go/clock/clock.go
@@ -4,6 +4,9 @@ import (
 	"fmt"
 )
 
+// MINUTESINADAY gives the number of minutes in one day
+const MINUTESINADAY = 1440
+
 // Clock represent a real clock with minute and hour
 type Clock struct {
 	minute int
@@ -18,36 +21,21 @@ func New(h, m int) Clock {
 	return Clock{minute: NormaliseClock(m + h*60)}
 }
 
-// Add will plus a certain minutes to a clock
+// Add will add a certain minutes to a clock
 func (c Clock) Add(minutes int) Clock {
-	c.minute = NormaliseClock(c.minute + minutes)
-	return c
+	return New(0, c.minute+minutes)
 }
 
-// Subtract take away x minutes from a clock
+// Subtract takes away x minutes from a clock
 func (c Clock) Subtract(minutes int) Clock {
-	c.minute = NormaliseClock(c.minute - minutes)
-	return c
+	return New(0, c.minute-minutes)
 }
 
 // NormaliseClock normalise a clock in the right format
 func NormaliseClock(m int) int {
-	hour := m / 60
-	minute := m % 60
-
-	if minute < 0 {
-		hour = hour - 1 - (-minute)/60
-		minute = 60 - (-minute)%60
+	if m < 0 {
+		m = m % MINUTESINADAY
+		m = MINUTESINADAY + m
 	}
-	if hour < 0 {
-		hour = 24 - (-hour)%24
-	}
-	if minute >= 60 {
-		hour = hour + m/60
-		minute = minute % 60
-	}
-	if hour >= 24 {
-		hour = hour % 24
-	}
-	return hour*60 + minute
+	return m % MINUTESINADAY
 }
