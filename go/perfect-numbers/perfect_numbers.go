706b8f5e63e352ac35ce8f5bf349196176238c26
Will Shen <will.shen@anz.com>
2020-03-03 11:08:06 +1100

    refactoring

--- go/perfect-numbers/perfect_numbers.go
+++ go/perfect-numbers/perfect_numbers.go
@@ -2,16 +2,15 @@ package perfect
 
 import "errors"
 
-type Classification struct {
-	name string
-}
+type Classification int
 
-var (
-	ClassificationPerfect   = Classification{name: "ClassificationPerfect"}
-	ClassificationAbundant  = Classification{name: "ClassificationAbundant"}
-	ClassificationDeficient = Classification{name: "ClassificationDeficient"}
-	ErrOnlyPositive         = errors.New("Error, only natural numbers are allowed")
+const (
+	ClassificationPerfect Classification = iota
+	ClassificationAbundant
+	ClassificationDeficient
 )
+
+var ErrOnlyPositive = errors.New("Error, only natural numbers are allowed")
 
 // Classify determines if a natural number is a perfect , abundant or deficient number
 func Classify(num int64) (Classification, error) {
@@ -19,7 +18,7 @@ func Classify(num int64) (Classification, error) {
 	sum := Sum(factors)
 
 	if num <= 0 {
-		return Classification{}, ErrOnlyPositive
+		return -1, ErrOnlyPositive
 	}
 	if num == 1 {
 		return ClassificationDeficient, nil
@@ -37,16 +36,8 @@ func Classify(num int64) (Classification, error) {
 func Factors(num int64) []int64 {
 	var result []int64
 	result = append(result, 1)
-	// instead of going through the whole array, we only need to go through
-	// half of the items
-	var midPoint int64
 
-	if num%2 == 0 {
-		midPoint = num / 2
-	} else {
-		midPoint = (num + 1) / 2
-	}
-	for i := 2; int64(i) <= midPoint; i++ {
+	for i := 2; int64(i) <= num/2; i++ {
 		factor := num % int64(i)
 		if factor == 0 {
 			result = append(result, factor, int64(i))
