abe58c56a6f4904ee3f3df19fb1dfa3b21b08981
Will Shen <will.shen@anz.com>
2020-03-04 10:36:01 +1100

    refactored build function to be simpler

--- go/tree-building/tree_building.go
+++ go/tree-building/tree_building.go
@@ -19,25 +19,6 @@ type Node struct {
 	Children []*Node
 }
 
-// Len() returns the length of the records
-// It implements the sort interface in Go
-func (r Records) Len() int {
-	return len(r)
-}
-
-// Swap is an implementation of the sort interface method
-// It implements the sort interfaces in Go
-func (r Records) Swap(i, j int) {
-	r[i], r[j] = r[j], r[i]
-}
-
-// Less reports whether the element with index i should sort before the element with index j.
-// We sort it based on lower Parent IDs first, so they can be inserted in order
-// It implements the sort interfaces in Go
-func (r Records) Less(i, j int) bool {
-	return r[i].ID < r[j].ID
-}
-
 // NewNode returns an node with empty value
 func NewNode() *Node { return &Node{} }
 
@@ -46,27 +27,14 @@ func Build(records []Record) (*Node, error) {
 	if len(records) == 0 {
 		return nil, nil
 	}
-
-	//sort the records based on increasing ID order
-	sort.Sort(Records(records))
-	// stores the records in a map for easy retrival
+	sort.Slice(records, func(i, j int) bool { return records[i].ID < records[j].ID })
 	treeMap := make(map[int]*Node, len(records))
-	var root *Node
 	for i, v := range records {
-		if i == 0 && v.ID != 0 {
-			return nil, errors.New("No root node exist")
+		if i == 0 && v.ID != 0 && v.Parent != 0 {
+			return nil, errors.New("Error with the root node record")
 		}
-		if i == 0 && v.Parent != 0 {
-			return nil, errors.New("Root should not have a parent")
-		}
-		if v.ID != 0 && v.ID == v.Parent {
-			return nil, errors.New("Record ID and parent ID can't be the same")
-		}
-		if i != v.ID {
-			return nil, errors.New("Non-continuous record found")
-		}
-		if v.ID < v.Parent {
-			return nil, errors.New(("Record ID can't be lower than its parent ID"))
+		if v.ID != 0 && v.ID == v.Parent || v.ID < v.Parent || i != v.ID {
+			return nil, errors.New("Record ID error")
 		}
 		if found := treeMap[v.ID]; found != nil {
 			return nil, errors.New("Duplicate record found")
@@ -74,12 +42,10 @@ func Build(records []Record) (*Node, error) {
 		newNode := NewNode()
 		newNode.ID = v.ID
 		treeMap[v.ID] = newNode
-		if v.ID == 0 {
-			root = newNode
-		} else if v.ID != 0 {
+		if v.ID != 0 {
 			parentNode := treeMap[v.Parent]
 			parentNode.Children = append(parentNode.Children, newNode)
 		}
 	}
-	return root, nil
+	return treeMap[0], nil
 }
