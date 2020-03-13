60bc2b9d4c1b95171044f05be2bc23cb68588e08
Will Shen <will.shen@anz.com>
2020-03-13 14:39:42 +1100

    finished robot exercise

---
+++ go/robot-name/robot_name.go
@@ -0,0 +1,62 @@
+package robotname
+
+import (
+	"errors"
+	"math/rand"
+	"strconv"
+	"time"
+)
+
+type Robot struct {
+	name string
+}
+
+var robots = InitialiseRobotNames()
+
+// Name return a new name for the robot
+func (r *Robot) Name() (string, error) {
+	if r.name == "" {
+		if len(robots) == 0 { // exhausted all names and therefore start all over again
+			return "", errors.New("All names have been exhausted")
+		} else {
+			randomNumIndex := randomInt(0, len(robots))
+			r.name = robots[randomNumIndex]
+			robots = RemoveIndex(robots, randomNumIndex)
+		}
+	}
+	return r.name, nil
+}
+
+// Reset resets a robot's name
+func (r *Robot) Reset() string {
+	r.name = ""
+	return r.name
+}
+
+// InitialiseRobotNames generates a slice with all possible robot combinations
+// when we randomly select a robot name, we just choose a random number and use it as an index
+func InitialiseRobotNames() []string {
+	var robots []string
+	for i := 65; i < 91; i++ {
+		for j := 65; j < 91; j++ {
+			for k := 0; k < 10; k++ {
+				for l := 0; l < 10; l++ {
+					for m := 0; m < 10; m++ {
+						newRobot := string(byte(i)) + string(byte(j)) + strconv.Itoa(k) + strconv.Itoa(l) + strconv.Itoa(m)
+						robots = append(robots, newRobot)
+					}
+				}
+			}
+		}
+	}
+	return robots
+}
+
+func randomInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
+	return min + rand.Intn(max-min)
+}
+
+func RemoveIndex(s []string, index int) []string {
+	return append(s[:index], s[index+1:]...)
+}
