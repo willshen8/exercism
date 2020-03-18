720966ec1334e45a15db096f472564e7fd483224
Will Shen <will.shen@anz.com>
2020-03-18 21:58:44 +1100

    refactor code based review comments

--- go/robot-name/robot_name.go
+++ go/robot-name/robot_name.go
@@ -17,16 +17,18 @@ var usedRobotNames = make(map[string]bool)
 
 // Name return a new name for the robot
 func (r *Robot) Name() (string, error) {
-	if r.name == "" {
-		r.name = NewName()
-		for usedRobotNames[r.name] {
-			r.name = NewName()
-			if len(usedRobotNames) == maxRobots {
-				return "", errors.New("All names have been exhausted")
-			}
-		}
-		usedRobotNames[r.name] = true
+	if r.name != "" {
+		return r.name, nil
 	}
+	if len(usedRobotNames) == maxRobots {
+		return "", errors.New("All names have been exhausted")
+	}
+	r.name = NewName()
+	for usedRobotNames[r.name] {
+		r.name = NewName()
+
+	}
+	usedRobotNames[r.name] = true
 	return r.name, nil
 }
 
@@ -37,20 +39,11 @@ func (r *Robot) Reset() {
 
 // NewName generate a new robot name
 func NewName() string {
-	return randomString(2) + fmt.Sprintf("%03d", (randomInt(0, 1000)))
+	return string(rand.Intn(26)+'A') + string(rand.Intn(26)+'A') + fmt.Sprintf("%03d", (randomInt(0, 1000)))
 }
 
 // randomInt generate a random number between min and max
 func randomInt(min, max int) int {
 	r := rand.New(rand.NewSource(time.Now().UnixNano()))
 	return min + r.Intn(max-min)
-}
-
-// Generate a random string of A-Z chars with length specified
-func randomString(length int) string {
-	bytes := make([]byte, length)
-	for i := 0; i < length; i++ {
-		bytes[i] = byte(randomInt(65, 91))
-	}
-	return string(bytes)
 }
