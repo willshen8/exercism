4da5399840953a901284d983b918258cdf73572d
Will Shen <will.shen@anz.com>
2020-04-02 11:40:20 +1100

    Change map style from pointer to non-pointer, so initalising is not needed

--- go/tournament/tournament.go
+++ go/tournament/tournament.go
@@ -2,17 +2,11 @@ package tournament
 
 import (
 	"bufio"
-	"errors"
 	"fmt"
 	"io"
-	"regexp"
 	"sort"
 	"strings"
 )
-
-// ErrInput throws an error when inputs has error and can't processed
-var ErrInput = errors.New("Error with inputs")
-var commentRegex = regexp.MustCompile(`#`)
 
 // Team struct records team's records
 type Result struct {
@@ -23,84 +17,72 @@ type Result struct {
 	points int
 }
 
+// TeamPoints is a struct used to help with sorting by both team and points
+type TeamPoints struct {
+	team   string
+	points int
+}
+
+// SortedTeams sort a map of teams based on points and alphabetical order
+func SortTeams(teamResults map[string]Result) []TeamPoints {
+	teamPoints := make([]TeamPoints, 0, len(teamResults))
+	for team, result := range teamResults {
+		teamPoints = append(teamPoints, TeamPoints{team: team, points: result.points})
+	}
+	sort.Slice(teamPoints, func(i, j int) bool { return teamPoints[i].team < teamPoints[j].team })
+	sort.Slice(teamPoints, func(i, j int) bool { return teamPoints[i].points > teamPoints[j].points })
+	return teamPoints
+}
+
 // Tally takes an input from io.reader and outputs a team's results in table format via io.writer
 func Tally(r io.Reader, w io.Writer) error {
-	teamResults := make(map[string]*Result)
+	teamResults := make(map[string]Result)
 	scanner := bufio.NewScanner(r)
 	for scanner.Scan() {
 		line := scanner.Text()
-		if commentRegex.MatchString(line) || line == "" {
+		if strings.HasPrefix(line, "#") || line == "" {
 			continue
 		}
 
 		results := strings.Split(line, ";")
 		if len(results) != 3 {
-			return ErrInput
+			return fmt.Errorf("Error with reading input, received: %s", line)
 		}
 		teamA := results[0]
 		teamB := results[1]
 		result := results[2]
 
-		if _, ok := teamResults[teamA]; !ok {
-			teamResults[teamA] = &Result{}
-		}
-		if _, ok := teamResults[teamB]; !ok {
-			teamResults[teamB] = &Result{}
-		}
-		teamResults[teamA].mp++
-		teamResults[teamB].mp++
+		a, b := teamResults[teamA], teamResults[teamB]
+		a.mp++
+		b.mp++
 
 		switch result {
 		case "win":
-			teamResults[teamA].points += 3
-			teamResults[teamA].wins++
-			teamResults[teamB].losses++
+			a.points += 3
+			a.wins++
+			b.losses++
 		case "loss":
-			teamResults[teamA].losses++
-			teamResults[teamB].wins++
-			teamResults[teamB].points += 3
+			a.losses++
+			b.wins++
+			b.points += 3
 		case "draw":
-			teamResults[teamA].draws++
-			teamResults[teamA].points++
-			teamResults[teamB].draws++
-			teamResults[teamB].points++
+			a.draws++
+			a.points++
+			b.draws++
+			b.points++
 		default:
-			return ErrInput
+			return fmt.Errorf("Error with game result, it should be win, loss or draw, instead got: %s", result)
 		}
+		teamResults[teamA], teamResults[teamB] = a, b
 	}
 
 	sortedTeams := SortTeams(teamResults)
 
 	fmt.Fprintf(w, "%-31v|%3v |%3v |%3v |%3v |%3v\n", "Team", "MP", "W", "D", "L", "P")
-	for _, team := range sortedTeams {
-		result := teamResults[team]
+	for _, v := range sortedTeams {
+		result := teamResults[v.team]
 		fmt.Fprintf(w, "%-31v|%3v |%3v |%3v |%3v |%3v\n",
-			team, result.mp, result.wins, result.draws, result.losses, result.points)
+			v.team, result.mp, result.wins, result.draws, result.losses, result.points)
 	}
 	return nil
-}
-
-// SortedTeams sort a map of teams based on points and alphabetical order
-func SortTeams(teamResults map[string]*Result) []string {
-	teams := make([]string, 0, len(teamResults))
-	for team := range teamResults {
-		teams = append(teams, team)
-	}
-	sort.Strings(teams)
-
-	// using bubble sort to sort
-	for sorted := false; !sorted; {
-		swapped := false
-		for i := 0; i < len(teamResults)-1; i++ {
-			if teamResults[teams[i+1]].points > teamResults[teams[i]].points {
-				teams[i], teams[i+1] = teams[i+1], teams[i]
-				swapped = true
-			}
-		}
-		if !swapped {
-			sorted = true
-		}
-
-	}
-	return teams
 }
