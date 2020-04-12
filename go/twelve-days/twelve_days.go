27538a1961616f969ada62163d2b3c4720b1516d
Will Shen <will.shen@anz.com>
2020-04-12 11:35:13 +1000

    simplify tweleve days by adding all phrases into one constant

--- go/twelve-days/twelve_days.go
+++ go/twelve-days/twelve_days.go
@@ -1,54 +1,28 @@
 package twelve
 
 import (
-	"fmt"
 	"strings"
 )
 
-type verse struct {
-	verseNum string
-	verse    string
-}
-
-var (
-	verses = []verse{
-		{verseNum: "first", verse: "a Partridge in a Pear Tree"},
-		{verseNum: "second", verse: "two Turtle Doves"},
-		{verseNum: "third", verse: "three French Hens"},
-		{verseNum: "fourth", verse: "four Calling Birds"},
-		{verseNum: "fifth", verse: "five Gold Rings"},
-		{verseNum: "sixth", verse: "six Geese-a-Laying"},
-		{verseNum: "seventh", verse: "seven Swans-a-Swimming"},
-		{verseNum: "eighth", verse: "eight Maids-a-Milking"},
-		{verseNum: "ninth", verse: "nine Ladies Dancing"},
-		{verseNum: "tenth", verse: "ten Lords-a-Leaping"},
-		{verseNum: "eleventh", verse: "eleven Pipers Piping"},
-		{verseNum: "twelfth", verse: "twelve Drummers Drumming"},
-	}
-)
+const song = "On the first day of Christmas my true love gave to me: a Partridge in a Pear Tree.\n" +
+	"On the second day of Christmas my true love gave to me: two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the third day of Christmas my true love gave to me: three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the fourth day of Christmas my true love gave to me: four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the fifth day of Christmas my true love gave to me: five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the sixth day of Christmas my true love gave to me: six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the seventh day of Christmas my true love gave to me: seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the eighth day of Christmas my true love gave to me: eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the ninth day of Christmas my true love gave to me: nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the tenth day of Christmas my true love gave to me: ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the eleventh day of Christmas my true love gave to me: eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree.\n" +
+	"On the twelfth day of Christmas my true love gave to me: twelve Drummers Drumming, eleven Pipers Piping, ten Lords-a-Leaping, nine Ladies Dancing, eight Maids-a-Milking, seven Swans-a-Swimming, six Geese-a-Laying, five Gold Rings, four Calling Birds, three French Hens, two Turtle Doves, and a Partridge in a Pear Tree."
 
 // Verse takes an integer n and output the corresponding verse of the day
 func Verse(n int) string {
-	baseVerse := fmt.Sprintf("On the %s day of Christmas my true love gave to me: ", verses[n-1].verseNum)
-	var returnVerse []string
-	for i := n - 1; i >= 0; i-- {
-		if i == 0 && n != 1 {
-			returnVerse = append(returnVerse, "and "+verses[i].verse)
-		} else if n == 1 {
-			returnVerse = append(returnVerse, verses[i].verse)
-		} else {
-			returnVerse = append(returnVerse, verses[i].verse)
-		}
-	}
-	return baseVerse + strings.Join(returnVerse, ", ") + "."
+	return strings.Split(song, "\n")[n-1]
 }
 
 // Song output the entires verses as a single string
 func Song() string {
-	var verses []string
-	for i := 1; i <= 12; i++ {
-		verses = append(verses, Verse(i))
-
-	}
-	return strings.Join(verses, "\n")
+	return song
 }
