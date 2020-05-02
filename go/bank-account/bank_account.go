279f9a4677f5745e622600f0847aab4f2d263430
Will Shen <will.shen@anz.com>
2020-05-02 15:49:56 +1000

    modify balance() to have mutex as well to pass test race

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -4,12 +4,14 @@ import (
 	"sync"
 )
 
+// Account represent a person's bank account
 type Account struct {
 	amount int64
 	open   bool
 	mutex  *sync.Mutex
 }
 
+// Open creates a new account with some initial money
 func Open(initialDeposit int64) *Account {
 	if initialDeposit < 0 {
 		return nil
@@ -17,6 +19,7 @@ func Open(initialDeposit int64) *Account {
 	return &Account{amount: initialDeposit, open: true, mutex: &sync.Mutex{}}
 }
 
+// Close shuts down an account by paying out the balance and return a boolean
 func (a *Account) Close() (payout int64, ok bool) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
@@ -29,13 +32,17 @@ func (a *Account) Close() (payout int64, ok bool) {
 	return payout, true
 }
 
+// Balance outputs the current account balance
 func (a *Account) Balance() (balance int64, ok bool) {
+	a.mutex.Lock()
+	defer a.mutex.Unlock()
 	if !a.open {
 		return 0, false
 	}
 	return a.amount, true
 }
 
+// Deposit allows user to withdraw or deposit money into account
 func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
 	a.mutex.Lock()
 	defer a.mutex.Unlock()
