47bcadb2eca6f79de5692218bb79523836e686fc
Will Shen <will.shen@anz.com>
2020-05-02 20:26:40 +1000

    refactor code after review

--- go/bank-account/bank_account.go
+++ go/bank-account/bank_account.go
@@ -6,52 +6,52 @@ import (
 
 // Account represent a person's bank account
 type Account struct {
-	amount int64
-	open   bool
-	mutex  *sync.Mutex
+	sync.RWMutex
+	balance int64
+	close   bool
 }
 
-// Open creates a new account with some initial money
+// close creates a new account with some initial money
 func Open(initialDeposit int64) *Account {
 	if initialDeposit < 0 {
 		return nil
 	}
-	return &Account{amount: initialDeposit, open: true, mutex: &sync.Mutex{}}
+	return &Account{balance: initialDeposit}
 }
 
 // Close shuts down an account by paying out the balance and return a boolean
 func (a *Account) Close() (payout int64, ok bool) {
-	a.mutex.Lock()
-	defer a.mutex.Unlock()
-	if !a.open {
+	a.Lock()
+	defer a.Unlock()
+	if a.close {
 		return 0, false
 	}
-	payout = a.amount
-	a.amount = 0
-	a.open = false
+	payout = a.balance
+	a.balance = 0
+	a.close = true
 	return payout, true
 }
 
 // Balance outputs the current account balance
 func (a *Account) Balance() (balance int64, ok bool) {
-	a.mutex.Lock()
-	defer a.mutex.Unlock()
-	if !a.open {
+	a.Lock()
+	defer a.Unlock()
+	if a.close {
 		return 0, false
 	}
-	return a.amount, true
+	return a.balance, true
 }
 
 // Deposit allows user to withdraw or deposit money into account
-func (a *Account) Deposit(amount int64) (newBalance int64, ok bool) {
-	a.mutex.Lock()
-	defer a.mutex.Unlock()
-	if !a.open {
+func (a *Account) Deposit(balance int64) (newBalance int64, ok bool) {
+	a.Lock()
+	defer a.Unlock()
+	if a.close {
 		return 0, false
 	}
-	if a.amount+amount < 0 {
-		return a.amount, false
+	if a.balance+balance < 0 {
+		return a.balance, false
 	}
-	a.amount += amount
-	return a.amount, true
+	a.balance += balance
+	return a.balance, true
 }
