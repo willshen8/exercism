f2f28d749dd3c39e0d3cd90b6685987ee7c2e081
Will Shen <will.shen@anz.com>
2020-05-06 10:06:36 +1000

    remove bracket around parameter

--- javascript/resistor-color/resistor-color.js
+++ javascript/resistor-color/resistor-color.js
@@ -1,2 +1,2 @@
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
-export const colorCode = (color) => COLORS.indexOf(color)
+export const colorCode = color => COLORS.indexOf(color)
