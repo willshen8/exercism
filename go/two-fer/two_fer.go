e02ed0c79c8f7a9742a761aae377aa6d41e18b83
Will Shen <will.shen@anz.com>
2020-05-09 11:40:39 +1000

    refactor collatz exercise

--- go/two-fer/two_fer.go
+++ go/two-fer/two_fer.go
@@ -6,7 +6,7 @@ import "fmt"
 // ShareWith return a string based on the name input
 func ShareWith(name string) string {
 	if name == "" {
-		return "One for you, one for me."
+		name = "you"
 	}
 	return fmt.Sprintf("One for %s, one for me.", name)
 }
