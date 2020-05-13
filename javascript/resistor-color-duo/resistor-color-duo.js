c258de98c5c85847eade34d5e7388b638bb0db48
Will Shen <will.shen@anz.com>
2020-05-14 09:38:14 +1000

    refactor after review

--- javascript/resistor-color-duo/resistor-color-duo.js
+++ javascript/resistor-color-duo/resistor-color-duo.js
@@ -1,3 +1,3 @@
 export const COLORS = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
 export const colorCode = color => COLORS.indexOf(color)
-export const decodedValue = (colors) => parseInt(`${colorCode(colors[0])}${colorCode(colors[1])}`)
+export const decodedValue = colors => colorCode(colors[0]) * 10 + colorCode(colors[1])
