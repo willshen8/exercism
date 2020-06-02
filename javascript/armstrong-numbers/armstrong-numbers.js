c82ff8235bc5059570072555efb8cf261615ad37
Will Shen <will.shen@anz.com>
2020-06-03 09:53:57 +1000

    formatted for readability

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,1 +1,7 @@
-export const isArmstrongNumber = input => Array.from(input.toString()).map(Number).reduce((total, digit) => total + Math.pow(digit, input.toString().split('').length), 0) === input
+export const isArmstrongNumber = input =>
+    Array.from(input.toString())
+        .map(Number)
+        .reduce(
+            (total, digit) =>
+                total + Math.pow(digit, input.toString().split('').length),
+            0) === input
