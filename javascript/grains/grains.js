0eccfeb7b54bfdad73d8f4a0a53e7cc7e8ad0b3c
Will Shen <will.shen@anz.com>
2020-06-05 12:04:35 +1000

    finish grain exercise

---
+++ javascript/grains/grains.js
@@ -0,0 +1,6 @@
+export const square = squareNum => {
+  if (squareNum<1 || squareNum>64) throw new Error('square must be between 1 and 64')
+  return BigInt(Math.pow(2, squareNum-1))
+}
+
+export const total = () => BigInt(2 ** 64)-BigInt(1)
