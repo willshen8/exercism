9b2d82c4ac5e8bb1a0836a040364634e63e12c8f
Will Shen <will.shen@anz.com>
2020-06-05 15:00:45 +1000

    linting files

--- javascript/armstrong-numbers/armstrong-numbers.js
+++ javascript/armstrong-numbers/armstrong-numbers.js
@@ -1,7 +1,7 @@
 export const isArmstrongNumber = input =>
-    Array.from(input.toString())
-        .map(Number)
-        .reduce(
-            (total, digit) =>
-                total + Math.pow(digit, input.toString().split('').length),
-            0) === input
+  Array.from(input.toString())
+  .map(Number)
+  .reduce(
+    (total, digit) =>
+    total + Math.pow(digit, input.toString().split('').length),
+    0) === input
