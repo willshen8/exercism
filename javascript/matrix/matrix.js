859ce95f62ba66a614c64d05d398e4df26228de5
Will Shen <will.shen@anz.com>
2020-06-24 21:28:30 +1000

    update based on review comments

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -1,20 +1,24 @@
 export class Matrix {
-  constructor(input) {
-    this.matrix = input.split('\n').map(row => row.split(' ').map(Number))
+  constructor(data) {
+    this._data = data
+  }
+
+  static parseRows(input) {
+    return input.split('\n').map(row => row.split(' ').map(Number))
   }
 
   get rows() {
-    return this.matrix
+    if (!this._rows) {
+      this._rows = Matrix.parseRows(this._data)
+    }
+    return this._rows
   }
 
   get columns() {
-    if (this.matrix.length < 2) return this.matrix
-    const columns = new Array(this.matrix[0].length).fill(0).map(() => new Array(this.matrix.length).fill(0));
-    this.matrix.map((row, i) => {
-      row.map((e, j) => {
-        columns[j][i] = e
-      })
-    })
-    return columns
+    if (!this._rows) {
+      this._rows = Matrix.parseRows(this._data)
+    }
+    return this._rows[0].map(
+        (_, i) => this._rows.map((_, j) => this._rows[j][i]))
   }
 }
