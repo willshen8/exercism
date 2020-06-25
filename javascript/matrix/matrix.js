4d888aa6013e92f9deb7a6118017a303ae7ce74f
Will Shen <will.shen@anz.com>
2020-06-25 10:44:28 +1000

    make columns as static variables

--- javascript/matrix/matrix.js
+++ javascript/matrix/matrix.js
@@ -7,6 +7,10 @@ export class Matrix {
     return input.split('\n').map(row => row.split(' ').map(Number))
   }
 
+  static transpose(input) {
+    return input[0].map((_, i) => input.map(row => row[i]))
+  }
+
   get rows() {
     if (!this._rows) {
       this._rows = Matrix.parseRows(this._data)
@@ -15,10 +19,10 @@ export class Matrix {
   }
 
   get columns() {
-    if (!this._rows) {
-      this._rows = Matrix.parseRows(this._data)
+    if (!this._columns) {
+      if (!this._rows) this.rows
+      return Matrix.transpose(this._rows)
     }
-    return this._rows[0].map(
-        (_, i) => this._rows.map((_, j) => this._rows[j][i]))
+    return this._columns
   }
 }
