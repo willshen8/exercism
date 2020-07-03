df826973b4623ce94faecdeeb507a7aae1be37aa
Will Shen <will.shen@anz.com>
2020-07-03 23:19:44 +1000

    edit after mentor review

--- javascript/bob/bob.js
+++ javascript/bob/bob.js
@@ -1,10 +1,38 @@
 export const hey = (message) => {
   let trimmedMessage = message.trim()
-  if (trimmedMessage.replace(/[^a-zA-Z0-9!?]/g, "") == "") return "Fine. Be that way!"
-  if (/^[A-Z]+$/.test(trimmedMessage.replace(/[^a-zA-Z0-9]/g, "")) && trimmedMessage[trimmedMessage.length - 1] == '?') {
-    return "Calm down, I know what I'm doing!"
-  }
-  if (trimmedMessage[trimmedMessage.length - 1] == '?') return "Sure."
-  if (/^[A-Z]+$/.test(trimmedMessage.replace(/[^a-zA-Z]/g, ""))) return "Whoa, chill out!"
-  return "Whatever."
+  return SITUATIONS
+    .find(situation => situation.test(trimmedMessage))
+    .answer;
 }
+
+//  Helper functions
+const isShouting = message => /^[A-Z]+$/.test(message.replace(/[^a-zA-Z]/g, ""))
+const isSilence = message => message.replace(/[^a-zA-Z0-9!?]/g, "") === ""
+const isAsking = message => message[message.length - 1] === '?'
+
+// List of situations
+const SITUATIONS = [{
+    answer: "Fine. Be that way!",
+    test: isSilence
+  },
+  {
+    answer: "Calm down, I know what I'm doing!",
+    test: function (message) {
+      return isAsking(message) && isShouting(message)
+    }
+  },
+  {
+    answer: "Sure.",
+    test: isAsking
+  },
+  {
+    answer: "Whoa, chill out!",
+    test: isShouting
+  },
+  {
+    answer: "Whatever.",
+    test: function () {
+      return true
+    }
+  }
+]
