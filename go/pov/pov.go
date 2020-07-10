996b72523f7d57e6be10695e539b9d4c2e08ce04
Will Shen <will.shen@anz.com>
2020-07-10 16:48:00 +1000

    finished functions for constructing a graph

--- go/pov/pov.go
+++ go/pov/pov.go
@@ -9,48 +9,106 @@ type Node struct {
 	label string
 }
 
-type Arc struct {
-	from string
-	to   string
-}
-
 type Graph struct {
-	nodes []Node
-	arcs  []Arc
+	nodes []*Node
+	arcs  map[string][]string
 	lock  sync.RWMutex
 }
 
-func New() *Graph { return &Graph{} }
+type Arc struct {
+	from, to string
+}
+
+func New() *Graph {
+	return &Graph{arcs: make(map[string][]string)}
+}
 
 // AddNode adds a new node to the graph
 func (g *Graph) AddNode(nodeLabel string) {
 	g.lock.Lock()
 	defer g.lock.Unlock()
-	g.nodes = append(g.nodes, Node{label: nodeLabel})
+	g.nodes = append(g.nodes, &Node{label: nodeLabel})
 }
 
 // AddArc adds an arc/edge to the graph
 func (g *Graph) AddArc(from, to string) {
 	g.lock.Lock()
 	defer g.lock.Unlock()
-	g.arcs = append(g.arcs, Arc{from: from, to: to})
+	g.arcs[from] = append(g.arcs[from], to)
+}
+
+// RemoveArc removes an arc/edge from the graph
+func (g *Graph) RemoveArc(from, to string) {
+	g.lock.Lock()
+	defer g.lock.Unlock()
+	toArcs := g.arcs[from]
+	for i, v := range toArcs {
+		if v == to {
+			g.arcs[from][i] = g.arcs[from][len(g.arcs[from])-1]
+			g.arcs[from] = g.arcs[from][:len(g.arcs[from])-1]
+		}
+	}
 }
 
 // ArcList returns a dump of all the arcs in the graph
 func (g *Graph) ArcList() []string {
 	var arcList []string
-	for _, arc := range g.arcs {
-		arcList = append(arcList, fmt.Sprintf("%v -> %v", arc.from, arc.to))
+	for from, arcs := range g.arcs {
+		for _, to := range arcs {
+			arcList = append(arcList, fmt.Sprintf("%v -> %v", from, to))
+		}
 	}
 	return arcList
 }
 
-// Changeroot make the newRoot the newRoot and changes all directed arcs as well
+// FindPathVertices recursively finds a list of vertices between two vertices: from and to
+// and stores the results in a map called path
+func (g *Graph) FindPathVertices(from, to string) (path []Arc) {
+	keys := make([]string, 0, len(g.arcs))
+	for k := range g.arcs {
+		keys = append(keys, k)
+	}
+
+	var found string
+	if _, ok := g.arcs[to]; !ok {
+		return path
+	}
+	for _, k := range keys {
+		for _, arc := range g.arcs[k] {
+			if arc == to {
+				path = append(path, Arc{k, to})
+				found = k
+				fmt.Println("Found=", found)
+			}
+		}
+	}
+
+	// if found != from {
+	// 	path = g.FindPathVertices(from, found)
+	// }
+	return path
+}
+
+// ChangeRoot make the newRoot the newRoot and changes all directed arcs as well
 func (g *Graph) ChangeRoot(oldRoot, newRoot string) *Graph {
-	for i, v := range g.arcs {
-		if v.from == newRoot {
-			g.arcs[i].from, g.arcs[i].to = g.arcs[i].to, g.arcs[i].from
+	// first find all the nodes start from oldRoot to newRoot
+	path := g.FindPathVertices(oldRoot, newRoot)
+	fmt.Println("Path:", path)
+	if verticesTo, ok := g.arcs[oldRoot]; ok {
+		for _, v := range verticesTo {
+			if v == newRoot {
+				g.AddArc(newRoot, oldRoot)
+				g.RemoveArc(oldRoot, newRoot)
+			}
 		}
 	}
 	return g
 }
+
+// FindPathBetweenTwoVertices returns a list of all nodes between two vertices
+// func (g *Graph) FindPathBetweenTwoVertices(from, to string) []string {
+// 	var pathVertices []string
+// 	for k, _ := range g.arcs[{
+// 		pathVertices = append(pathVertices, k)
+// 	}
+// }
