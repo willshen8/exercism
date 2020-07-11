5c585958eaa7d7baa1442678cb11dff0d02139f0
Will Shen <will.shen@anz.com>
2020-07-12 00:12:13 +1000

    refactor to compare the string after convert them into slice

--- go/anagram/anagram.go
+++ go/anagram/anagram.go
@@ -2,36 +2,25 @@ package anagram
 
 import (
 	"reflect"
+	"sort"
 	"strings"
 )
 
 // Detect returns a slice of strings from candidates that is an anagram of subject
 func Detect(subject string, candidates []string) []string {
 	var results []string
-	subjectMap := convertStringToCharMap(subject)
+	subjectSlice := strings.Split(strings.ToLower(subject), "")
+	sort.Strings(subjectSlice)
 
 	for _, candidate := range candidates {
 		if strings.EqualFold(subject, candidate) {
 			break
 		}
-		candidateMap := convertStringToCharMap(candidate)
-		if reflect.DeepEqual(subjectMap, candidateMap) {
+		candidateSlice := strings.Split(strings.ToLower(candidate), "")
+		sort.Strings(candidateSlice)
+		if reflect.DeepEqual(subjectSlice, candidateSlice) {
 			results = append(results, candidate)
 		}
 	}
 	return results
-}
-
-// convertStringToCharMap convert a string into map with its char and its frequencies
-func convertStringToCharMap(str string) map[string]int {
-	strSlice := strings.Split(strings.ToLower(str), "")
-	charMap := make(map[string]int)
-	for _, v := range strSlice {
-		if _, ok := charMap[v]; !ok {
-			charMap[v] = 1
-		} else {
-			charMap[v]++
-		}
-	}
-	return charMap
 }
