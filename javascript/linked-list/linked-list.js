6e1f03f1c818472ffa7da73e77e4dc83b7468515
Will Shen <will.shen@anz.com>
2020-08-15 11:43:23 +1000

    add abstraction to delete function as per review comments

--- javascript/linked-list/linked-list.js
+++ javascript/linked-list/linked-list.js
@@ -1,6 +1,6 @@
 export class LinkedList {
   constructor() {
-    this.head = null
+    this.head = null 
     this.tail = null
   }
 
@@ -9,37 +9,38 @@ export class LinkedList {
     const newNode = new Node(nodeValue)
     const oldTail = this.tail
     if (!this.head) {
-      this.head = newNode
+      this.head = newNode 
       this.tail = newNode
-    } else {
+    }
+    else {
       oldTail.next = newNode
-      newNode.prev = oldTail
+      newNode.prev = oldTail 
       this.tail = newNode
     }
   }
 
   // Pop removes the element at back
   pop() {
-    if (!this.tail) return
+    if (!this.tail) return 
     const popNode = this.tail
-    if (this.head !== this.tail) {
-      const newTail = this.tail.prev
-      newTail.next = null
-      this.tail = newTail
-    } else {
-      this.head = null
+      if (this.head !== this.tail) {
+        const newTail = this.tail.prev
+        newTail.next = null 
+        this.tail = newTail
+      }
+    else {
+      this.head = null 
       this.tail = null
     }
     return popNode.data
   }
 
-  // shift removes the element from the beginning of the list 
+  // shift removes the element from the beginning of the list
   shift() {
     const shiftedNode = this.head
     const newHead = this.head.next
-    if (!this.head) return
-    if (!newHead)
-      this.tail = newHead
+    if (!this.head) return 
+    if (!newHead) this.tail = newHead 
     this.head = newHead
     return shiftedNode.data
   }
@@ -49,41 +50,20 @@ export class LinkedList {
     const newNode = new Node(nodeValue)
     const oldHead = this.head
     if (!this.head) {
-      this.head = newNode
+      this.head = newNode 
       this.tail = newNode
-    } else {
+    }
+    else {
       oldHead.prev = newNode
-      newNode.next = oldHead
+      newNode.next = oldHead 
       this.head = newNode
     }
   }
 
   // delete the first occurrence of a specified value
   delete(deleteValue) {
-    let deleteNode = null
-    let nextNode = null
-    let prevNode = null
-    if (this.head) {
-      deleteNode = this.head
-      while (deleteNode.data !== deleteValue) {
-        if (deleteNode.next === null) return
-        deleteNode = deleteNode.next
-      }
-    }
-
-    if (deleteNode == this.head) {
-      this.head = deleteNode.next
-      this.tail = deleteNode.next
-    } else if (deleteNode == this.tail) {
-      prevNode = deleteNode.prev
-      prevNode.next = null
-      this.tail = prevNode
-    } else {
-      nextNode = deleteNode.next
-      prevNode = deleteNode.prev
-      prevNode.next = nextNode
-      nextNode.prev = prevNode
-    }
+    const deleteNode = this.findNodeByValue(deleteValue)
+    if (deleteNode) this.deleteNode(deleteNode)
   }
 
   // count output the number of nodes in the linked list
@@ -97,12 +77,46 @@ export class LinkedList {
     }
     return count
   }
+
+  // findNodeByValue returns the first node with the specific value
+  findNodeByValue(value) {
+    let searchNode = null
+    if (this.head) {
+      searchNode = this.head
+      while (searchNode.data !== value) {
+        if (searchNode.next === null) {
+            return
+        }
+        searchNode = searchNode.next
+      }
+      return searchNode
+    }
+  }
+
+  // deleteNode removes deleteNode from the likedList
+  deleteNode(deleteNode){
+    let nextNode = null
+    let prevNode = null
+    if (deleteNode == this.head) {
+      this.head = deleteNode.next 
+      this.tail = deleteNode.next
+    } else if (deleteNode == this.tail) {
+      prevNode = deleteNode.prev
+      prevNode.next = null 
+      this.tail = prevNode
+    } else {
+      nextNode = deleteNode.next
+      prevNode = deleteNode.prev
+      prevNode.next = nextNode
+      nextNode.prev = prevNode
+    }
+  }
 }
 
 class Node {
   constructor(data, prev = null, next = null) {
-    this.data = data
-    this.prev = prev
+    this.data = data 
+    this.prev = prev 
     this.next = next
   }
 }
