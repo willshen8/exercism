f0bfbb684021c5e68cc9837e9f43ec01342cd068
Will Shen <will.shen@anz.com>
2020-08-21 17:22:21 +1000

    refactor cipher

--- go/rotational-cipher/rotational_cipher.go
+++ go/rotational-cipher/rotational_cipher.go
@@ -1,19 +1,15 @@
 package rotationalcipher
 
+import "unicode"
+
 func RotationalCipher(input string, inputShiftKey int) string {
 	var result []byte
 	for _, v := range []byte(input) {
 		var rotatedCipher byte
-		if v >= 65 && v <= 90 { // uppercase
+		if unicode.IsLower(rune(v)) || unicode.IsUpper(rune(v)) {
 			rotatedCipher = v + byte(inputShiftKey%26)
-			if rotatedCipher > 90 {
-				rotatedCipher -= 26
-			}
-			result = append(result, rotatedCipher)
-			continue
-		} else if v >= 97 && v <= 122 { // lowercase
-			rotatedCipher = v + byte(inputShiftKey%26)
-			if rotatedCipher > 122 {
+			if rotatedCipher > 90 && unicode.IsUpper(rune(v)) ||
+				rotatedCipher > 122 && unicode.IsLower(rune(v)) {
 				rotatedCipher -= 26
 			}
 			result = append(result, rotatedCipher)
