797c56b187868b91de5d863a33d14d34f9460dec
Will Shen <will.shen@anz.com>
2020-09-14 15:57:18 +1000

    update function to only sort on add function

--- javascript/grade-school/grade-school.js
+++ javascript/grade-school/grade-school.js
@@ -13,14 +13,13 @@ export class GradeSchool {
     } else {
       this.school[grade] = [name]
     }
-    const sortObject = obj => Object.keys(obj).sort().reduce((res, key) => (res[key] = obj[key], res), {})
-    sortObject(this.school)
+    this.school[grade].sort()
   }
 
   grade(gradeNumber) {
     const rosterCopy = JSON.parse(JSON.stringify(this.school))
     if (rosterCopy[gradeNumber])
-      return rosterCopy[gradeNumber].sort()
+      return rosterCopy[gradeNumber]
     else
       return []
   }
