1b13d9d6f642575385acaff4e83d37e6cabf90b0
Will Shen <will.shen@anz.com>
2020-09-27 11:35:20 +1000

    refactor to remove the use of a map structure

--- javascript/word-count/word-count.js
+++ javascript/word-count/word-count.js
@@ -1,18 +1,13 @@
 export const countWords = words => {
-  let wordMap = new Map()
-  const wordsArray = words.split(/[, ]/)
-  wordsArray.map(word => {
+  const result = {}
+  words.split(/[, ]/).map(word => {
     let parsedWord = word.trim().toLowerCase().replace(/(^\W*)|(\W*$)/g, '')
     if (parsedWord === '')
       return
-    if (!wordMap.has(parsedWord))
-      wordMap.set(parsedWord, 1)
+    if (result[parsedWord])
+      result[parsedWord]++
     else
-      wordMap.set(parsedWord, wordMap.get(parsedWord) + 1)
+      result[parsedWord] = 1
   })
-  const wordFreq = {}
-  for (const [key, value] of wordMap.entries()) {
-    wordFreq[key] = value
-  }
-  return wordFreq
+  return result
 }
