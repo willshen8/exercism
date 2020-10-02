ffcb397e8f70b6e1679765fae7a610ff8917a564
Will Shen <will.shen@anz.com>
2020-10-02 22:50:10 +1000

    refactor to separate reader and wrtier

--- go/paasio/paasio.go
+++ go/paasio/paasio.go
@@ -5,57 +5,71 @@ import (
 	"sync"
 )
 
-type counter struct {
+type readCounter struct {
 	n      int64
 	nops   int
 	reader io.Reader
+	mutex  sync.Mutex
+}
+
+type writeCounter struct {
+	n      int64
+	nops   int
 	writer io.Writer
 	mutex  sync.Mutex
 }
 
-// ReadCount returns the number of bytes read and the number of operations
-func (c *counter) ReadCount() (n int64, nops int) {
-	return c.n, c.nops
-}
-
-// WriteCount returns the number of written bytes and the number of operations
-func (c *counter) WriteCount() (n int64, nops int) {
-	return c.n, c.nops
-}
-
-func (c *counter) Read(input []byte) (int, error) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	n, err := c.reader.Read(input)
-	if err != nil {
-		return 0, err
-	}
-	c.n += int64(n)
-	c.nops++
-	return n, nil
-}
-
-func (c *counter) Write(input []byte) (int, error) {
-	c.mutex.Lock()
-	defer c.mutex.Unlock()
-	n, err := c.writer.Write(input)
-	if err != nil {
-		return 0, err
-	}
-	c.n += int64(n)
-	c.nops++
-	return n, nil
+type readWriteCounter struct {
+	readCounter
+	writeCounter
 }
 
 func NewReadCounter(r io.Reader) ReadCounter {
-	return &counter{reader: r}
+	return &readCounter{reader: r}
+}
+
+// ReadCount returns the number of bytes read and the number of operations
+func (r *readCounter) ReadCount() (n int64, nops int) {
+	return r.n, r.nops
+}
+
+func (r *readCounter) Read(input []byte) (int, error) {
+	r.mutex.Lock()
+	defer r.mutex.Unlock()
+	n, err := r.reader.Read(input)
+	if err != nil {
+		return 0, err
+	}
+	r.n += int64(n)
+	r.nops++
+	return n, nil
 }
 
 // NewWriteCounter returns a new WriteCounter
 func NewWriteCounter(w io.Writer) WriteCounter {
-	return &counter{writer: w}
+	return &writeCounter{writer: w}
+}
+
+// WriteCount returns the number of written bytes and the number of operations
+func (w *writeCounter) WriteCount() (n int64, nops int) {
+	return w.n, w.nops
+}
+
+func (w *writeCounter) Write(input []byte) (int, error) {
+	w.mutex.Lock()
+	defer w.mutex.Unlock()
+	n, err := w.writer.Write(input)
+	if err != nil {
+		return 0, err
+	}
+	w.n += int64(n)
+	w.nops++
+	return n, nil
 }
 
 func NewReadWriteCounter(rw io.ReadWriter) ReadWriteCounter {
-	return &counter{reader: rw, writer: rw}
+	return &readWriteCounter{
+		readCounter{reader: rw},
+		writeCounter{writer: rw},
+	}
 }
