497c9f0f2d5b3d64c367e211c20cabf37f6ad430
Will Shen <will.shen@anz.com>
2020-10-02 22:26:36 +1000

    finish paasio.go

--- go/paasio/paasio.go
+++ go/paasio/paasio.go
@@ -1,16 +1,61 @@
 package paasio
 
-import "io"
+import (
+	"io"
+	"sync"
+)
 
-// NewWriteCounter returns a new WriteCounter
-func NewWriteCounter(w io.Writer) WriteCounter {
-	return WriteCounter{io.Writer: w}
+type counter struct {
+	n      int64
+	nops   int
+	reader io.Reader
+	writer io.Writer
+	mutex  sync.Mutex
+}
+
+// ReadCount returns the number of bytes read and the number of operations
+func (c *counter) ReadCount() (n int64, nops int) {
+	return c.n, c.nops
+}
+
+// WriteCount returns the number of written bytes and the number of operations
+func (c *counter) WriteCount() (n int64, nops int) {
+	return c.n, c.nops
+}
+
+func (c *counter) Read(input []byte) (int, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	n, err := c.reader.Read(input)
+	if err != nil {
+		return 0, err
+	}
+	c.n += int64(n)
+	c.nops++
+	return n, nil
+}
+
+func (c *counter) Write(input []byte) (int, error) {
+	c.mutex.Lock()
+	defer c.mutex.Unlock()
+	n, err := c.writer.Write(input)
+	if err != nil {
+		return 0, err
+	}
+	c.n += int64(n)
+	c.nops++
+	return n, nil
 }
 
 func NewReadCounter(r io.Reader) ReadCounter {
-	return ReadCounter{io.Reader: r}
+	return &counter{reader: r}
 }
 
-func NewReadWriteCounter(rw readWriter) {
+// NewWriteCounter returns a new WriteCounter
+func NewWriteCounter(w io.Writer) WriteCounter {
+	return &counter{writer: w}
+}
 
+func NewReadWriteCounter(rw io.ReadWriter) ReadWriteCounter {
+	return &counter{reader: rw, writer: rw}
 }
