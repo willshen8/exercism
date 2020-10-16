537f25a99c97b810bd6fb2cb086178d285a71b47
Will Shen <will.shen@anz.com>
2020-10-16 18:18:53 +1100

    finish the exercise except last test case

--- javascript/robot-name/robot-name.js
+++ javascript/robot-name/robot-name.js
@@ -1,6 +1,30 @@
-// This is only a SKELETON file for the 'Robot Name' exercise. It's been
-// provided as a convenience to get your started writing code faster.
+const ASCII_A = 65
+const ASCII_Z = 90
+const NUMBERS = 10
+const TOTAL_NUMBER_OF_NAMES = 26 * 26 * 10 * 10 * 10;
 
-export class Robot { }
+export class Robot {
+    constructor() {
+        this.usedNames = new Set()
+        this._name = this.generateRobotName()
+    }
 
-Robot.releaseNames = () => { };
+    get name() { return this._name }
+
+    generateRobotName = () => {
+        const alpha1 = String.fromCharCode(Math.floor(Math.random() * (ASCII_Z - ASCII_A) + ASCII_A))
+        const alpha2 = String.fromCharCode(Math.floor(Math.random() * (ASCII_Z - ASCII_A) + ASCII_A))
+        const num1 = Math.floor(Math.random() * NUMBERS)
+        const num2 = Math.floor(Math.random() * NUMBERS)
+        const num3 = Math.floor(Math.random() * NUMBERS)
+        if (this.usedNames.has(`${alpha1}${alpha2}${num1}${num2}${num3}`)) {
+            return this.generateRobotName()
+        }
+        this.usedNames.add(`${alpha1}${alpha2}${num1}${num2}${num3}`)
+        return alpha1 + alpha2 + num1 + num2 + num3
+    }
+
+    reset = () => this._name = this.generateRobotName()
+}
+
+Robot.releaseNames = () => {};
