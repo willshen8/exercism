842868ab419c5daec7b8ef879a306363683d01c6
Will Shen <will.shen@anz.com>
2020-10-25 15:13:34 +1100

    finish binary search

--- go/binary-search/binary_search.go
+++ go/binary-search/binary_search.go
@@ -1,1 +1,18 @@
 package binarysearch
+
+func SearchInts(input []int, x int) int {
+	var low = 0
+	var high = len(input) - 1
+
+	for low <= high {
+		var mid = low + (high-low)/2
+		if input[mid] == x {
+			return mid
+		} else if x > input[mid] {
+			low = mid + 1
+		} else if x < input[mid] {
+			high = mid - 1
+		}
+	}
+	return -1
+}
