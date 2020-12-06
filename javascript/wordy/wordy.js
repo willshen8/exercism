8698579f9fa176379ce9d03fe00b556823f0a172
Will Shen <will.shen@anz.com>
2020-12-06 12:05:29 +1100

    pass simple tests for simple operations involves one operation

--- javascript/wordy/wordy.js
+++ javascript/wordy/wordy.js
@@ -1,8 +1,32 @@
-//
-// This is only a SKELETON file for the 'Wordy' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+export const answer = question => {
+  const validOperators = ["multiplied", "divided", "minus", "plus"]
+  const isOperator = input => validOperators.includes(input)
+  const isOperand = input => Number.isInteger(parseInt(input))
+  const parsedOperands = []
+  const parsedOperators = []
+  const calculate = (operandA, operandB, operation) => {
+    console.log("Hello world: ", operandA, operandB)
+    switch(operation) {
+      case "multiplied":
+        return operandA * operandB
+      case "divided":
+        return operandA / operandB
+      case "minus":
+        return operandA - operandB
+      case "plus":
+        return operandA + operandB
+    }
+  }
 
-export const answer = () => {
-  throw new Error("Remove this statement and implement this function");
+  question.replace('?', '').split(" ").map(word => {
+    if(isOperand(word)) {
+      parsedOperands.push(parseInt(word))
+    }
+    if (isOperator(word)) {
+      parsedOperators.push(word)
+    }
+  })
+
+  if (parsedOperands.length === 1) return parsedOperands[0]
+  return calculate(parsedOperands[0], parsedOperands[1], parsedOperators[0])
 };
