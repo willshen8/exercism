b134246ce1d2f8bcd492311ce3de40ac4c87d6ad
Will Shen <william.shen8@gmail.com>
2021-06-19 13:38:57 +1000

    update based on feedbacks

--- typescript/resistor-color-duo/resistor-color-duo.ts
+++ typescript/resistor-color-duo/resistor-color-duo.ts
@@ -1,31 +1,26 @@
 export class ResistorColor {
-  private colors: string[]
-
-  constructor(colors: string[]) {
+  constructor(private colors: string[]) {
     if (colors.length < 2) {
       throw new Error('At least two colors need to be present')
     }
-    this.colors = colors
   }
   
   value():number {
-
-    return parseInt(this.colors.slice(0,2).reduce((accumulator:string, color:string) => accumulator + resistorColors.get(color),''))
+    return resistorColors.indexOf(this.colors[0]) * 10 + resistorColors.indexOf(this.colors[1])
   }
   
 }
 export default ResistorColor
 
-
-const resistorColors = new Map<string, string>([
-  ['black', '0'],
-  ['brown', '1'],
-  ['red', '2'],
-  ['orange', '3'],
-  ['yellow', '4'],
-  ['green', '5'],
-  ['blue', '6'],
-  ['violet', '7'],
-  ['grey', '8'],
-  ['white', '9'],
-])
+const resistorColors = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+]
