bcaa86af3ce3091160c6f3537e44980379e6082e
Will Shen <william.shen8@gmail.com>
2021-06-20 09:44:13 +1000

    finished leap

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,5 +1,8 @@
-function isLeapYear(/* Parameters go here */) {
-  // Your code here
+function isLeapYear(year:number) {
+  if(year % 400 === 0) return true
+  if(year % 100 === 0) return false
+  if(year % 4 === 0) return true
+  return false
 }
 
 export default isLeapYear
