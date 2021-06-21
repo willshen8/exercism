b3329ae0f732b36b4bcc8a440e2a5af2c147c9e0
Will Shen <william.shen8@gmail.com>
2021-06-22 09:33:33 +1000

    finish exercise

--- typescript/resistor-color/resistor-color.ts
+++ typescript/resistor-color/resistor-color.ts
@@ -1,5 +1,5 @@
-export const colorCode = () => {
-  throw new Error('Delete this line and implement this function')
-}
+export const COLORS:readonly string[] = ['black','brown','red','orange','yellow','green','blue','violet','grey','white']
 
-export const COLORS = undefined
+export const colorCode = (color:string):number => COLORS.indexOf(color)
+
+
