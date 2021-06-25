0c2aaa19aba3ec688adb9bd8cbc36316a96ca077
Will Shen <william.shen8@gmail.com>
2021-06-25 15:26:01 +1000

    finished transcription

--- typescript/rna-transcription/rna-transcription.ts
+++ typescript/rna-transcription/rna-transcription.ts
@@ -1,7 +1,18 @@
+const DNAtoRNA : { [key: string]: string } = {
+  'G':'C',
+  'C':'G',
+  'T':'A',
+  'A':'U',
+} as const
 class Transcriptor {
-  toRna(/* Parameters go here */) {
-    // Your code here
+  toRna(dnaStrand:string): string {
+    return dnaStrand.split('').map((item: string) => {
+      if (!DNAtoRNA[item]) throw new Error('Invalid input DNA.')
+      return DNAtoRNA[item]
+    }).join("")
   }
 }
+
+
 
 export default Transcriptor
