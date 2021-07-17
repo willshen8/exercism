169b31983827ab6de85622ba4c4f416f054a0ccf
Will Shen <william.shen8@gmail.com>
2021-07-17 16:43:11 +1000

    finished function

--- typescript/bob/bob.ts
+++ typescript/bob/bob.ts
@@ -1,7 +1,36 @@
+//  Helper functions
+const isShouting = (message:string) => message.toUpperCase() === message && /[A-Z]/.test(message)
+const isSilence = (message:string) => message === ""
+const isAsking = (message:string) => message[message.length - 1] === '?'
+
 class Bob {
-  hey(/* Parameters go here */) {
-    // Your code here
+  hey(comment:string):string {
+    return SITUATIONS.find(situation => situation.test(comment.trim())).answer;
   }
 }
 
+  // List of situations
+  const SITUATIONS:Array<any> = [{
+    answer: "Fine. Be that way!",
+    test: isSilence
+    },
+    {
+      answer: "Calm down, I know what I'm doing!",
+      test: (message:string) => isAsking(message) && isShouting(message)
+    },
+    {
+      answer: "Sure.",
+      test: isAsking
+    },
+    {
+      answer: "Whoa, chill out!",
+      test: isShouting
+    },
+    {
+      answer: "Whatever.",
+      test: () => true
+    }
+  ]
+
 export default Bob
+
