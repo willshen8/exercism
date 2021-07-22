79b8a62f7491828acf91ff864f0212fe0e6ec32f
Will Shen <william.shen8@gmail.com>
2021-07-22 10:34:11 +1000

    finish matrix

--- typescript/matrix/matrix.ts
+++ typescript/matrix/matrix.ts
@@ -1,6 +1,22 @@
 class Matrix {
-  constructor(/* Parameters go here */) {
-    // Your code here
+  private _rows: number[][] = Matrix.parseRows(this._data)
+  private _columns: number[][] = Matrix.transpose(this._rows);
+  constructor(private _data: string) { }
+
+  static parseRows(input: string): number[][] {
+    return input.split('\n').map(row => row.split(' ').map(Number))
+  }
+
+  static transpose(input: number[][]): number[][] {
+    return input[0].map((_, i) => input.map(row => row[i]))
+  }
+
+  public get rows(): number[][] {
+    return this._rows
+  }
+
+  public get columns(): number[][] {
+    return this._columns
   }
 }
 
