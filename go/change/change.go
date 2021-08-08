c770d7d04723610a4e74ce2095db5ef0d4267a7b
Will Shen <william.shen8@gmail.com>
2021-08-08 16:49:25 +1000

    completed the exercise

--- go/change/change.go
+++ go/change/change.go
@@ -5,7 +5,12 @@ import (
 	"sort"
 )
 
-func Change(coins []int, target int, resultsCache map[int][]int) ([]int, error) {
+func Change(coins []int, target int) ([]int, error) {
+	cache := make(map[int][]int, target)
+	return FindChangeCombinations(coins, target, cache)
+}
+
+func FindChangeCombinations(coins []int, target int, resultsCache map[int][]int) ([]int, error) {
 	if val, ok := resultsCache[target]; ok {
 		return val, nil
 	}
@@ -15,14 +20,13 @@ func Change(coins []int, target int, resultsCache map[int][]int) ([]int, error) 
 	if target < 0 {
 		return nil, errors.New("Target can't be less than zero")
 	}
-
 	var leastNumOfCoinChangeCombinations []int
 	for _, coin := range coins {
 		remainder := target - coin
-		remainderCombination, _ := Change(coins, remainder, resultsCache)
+		remainderCombination, _ := FindChangeCombinations(coins, remainder, resultsCache)
 
 		if remainderCombination != nil {
-			combination := append(remainderCombination, coin)
+			combination := append([]int{coin}, remainderCombination...)
 			if leastNumOfCoinChangeCombinations == nil || len(combination) < len(leastNumOfCoinChangeCombinations) {
 				leastNumOfCoinChangeCombinations = combination
 			}
