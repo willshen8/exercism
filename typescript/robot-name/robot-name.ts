7369de751d12ce838a06b5adc835e31d538e971d
Will Shen <william.shen8@gmail.com>
2021-08-15 11:36:07 +1000

    finish exercise

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,15 +1,41 @@
+const ASCII_A = 65
+const ASCII_Z = 90
+const MAX_NUM = 1000
+const generateRandomLetter = ():string => String.fromCharCode(Math.floor(Math.random() * (ASCII_Z - ASCII_A) + ASCII_A))
+const generateRandomNumber = ():number => Math.floor(Math.random() * MAX_NUM)
+const usedNames = new Set();
 export default class Robot {
-  constructor() {}
+  _name: string
+  
+  constructor() {
+    this._name = this.generateUniqueRobotName()
+  }
+
+  generateUniqueRobotName = (): string => {
+    let newRobotName = this.NewRobotName()
+    do {
+      newRobotName = this.NewRobotName()
+    } while (usedNames.has(newRobotName))
+    usedNames.add(newRobotName)
+    return newRobotName
+  }
+
+  NewRobotName = ():string => {
+    let randomLetter1 = generateRandomLetter()
+    let randomLetter2 = generateRandomLetter()
+    let randomNum = generateRandomNumber().toString().padStart(3, "0")
+    return `${randomLetter1}${randomLetter2}${randomNum}`
+  }
 
   public get name(): string {
-    throw new Error('Implement Robot#name')
+    return this._name
   }
 
   public resetName(): void {
-    throw new Error('Implement Robot#resetName')
+    this._name = this.generateUniqueRobotName()
   }
 
   public static releaseNames(): void {
-    throw new Error('Implement Robot.releaseNames')
+    usedNames.clear()
   }
 }
