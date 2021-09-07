581154d9842bd5679cb80c78c208664e80598d7a
Will Shen <william.shen8@gmail.com>
2021-09-07 20:21:09 +1000

    refactor to pass all tests

--- typescript/robot-name/robot-name.ts
+++ typescript/robot-name/robot-name.ts
@@ -1,17 +1,17 @@
-const ASCII_A = 65
-const ASCII_Z = 90
+const ASCII_A = "A".charCodeAt(0)
+const ASCII_Z = "Z".charCodeAt(0)
 const MAX_NUM = 1000
-const generateRandomLetter = ():string => String.fromCharCode(Math.floor(Math.random() * (ASCII_Z - ASCII_A) + ASCII_A))
-const generateRandomNumber = ():number => Math.floor(Math.random() * MAX_NUM)
+const generateRandomLetter = (): string => String.fromCharCode(Math.floor(Math.random() * (ASCII_Z - ASCII_A + 1) + ASCII_A))
+const generateRandomNumber = (): number => Math.floor(Math.random() * MAX_NUM)
 const usedNames = new Set();
 export default class Robot {
-  _name: string
-  
+  private _name: string
+
   constructor() {
     this._name = this.generateUniqueRobotName()
   }
 
-  generateUniqueRobotName = (): string => {
+  private generateUniqueRobotName(): string {
     let newRobotName = this.NewRobotName()
     do {
       newRobotName = this.NewRobotName()
@@ -20,10 +20,10 @@ export default class Robot {
     return newRobotName
   }
 
-  NewRobotName = ():string => {
-    let randomLetter1 = generateRandomLetter()
-    let randomLetter2 = generateRandomLetter()
-    let randomNum = generateRandomNumber().toString().padStart(3, "0")
+  private NewRobotName(): string {
+    const randomLetter1: string = generateRandomLetter()
+    const randomLetter2: string = generateRandomLetter()
+    const randomNum: string = generateRandomNumber().toString().padStart(3, "0")
     return `${randomLetter1}${randomLetter2}${randomNum}`
   }
 
