621413fd432f4d5b9b3c1d33e0d467b2d39f102f
Will Shen <william.shen8@gmail.com>
2021-09-16 20:40:01 +1000

    update discard function to use keep function

--- typescript/strain/strain.ts
+++ typescript/strain/strain.ts
@@ -8,12 +8,6 @@ export const keep = <T>(inputList: T[], predicate: (item: T) => boolean): T[] =>
   return output
 }
 
-export function discard<T>(inputList: T[], predicate: (item: T) => boolean): T[] {
-  let output: T[] = []
-  for (let listItem of inputList) {
-    if (!predicate(listItem)) {
-      output.push(listItem)
-    }
-  }
-  return output
+export const discard = <T>(inputList: T[], predicate: (item: T) => boolean): T[] => {
+  return keep(inputList, function (item: T) { return !predicate(item) })
 }
