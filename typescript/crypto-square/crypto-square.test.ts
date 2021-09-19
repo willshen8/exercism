209870680acede2a8e7e9a845327fafc496a6f3f
Will Shen <william.shen8@gmail.com>
2021-09-19 17:15:43 +1000

    passes all tests and refactoring is next

--- typescript/crypto-square/crypto-square.test.ts
+++ typescript/crypto-square/crypto-square.test.ts
@@ -21,17 +21,17 @@ describe('Crypto', () => {
     expect(crypto.ciphertext).toEqual('1')
   })
 
-  xtest('9 character plaintext results in 3 chunks of 3 characters', () => {
+  test('9 character plaintext results in 3 chunks of 3 characters', () => {
     const crypto = new Crypto('This is fun!')
     expect(crypto.ciphertext).toEqual('tsf hiu isn')
   })
 
-  xtest('8 character plaintext results in 3 chunks, the last one with a trailing space', () => {
+  test('8 character plaintext results in 3 chunks, the last one with a trailing space', () => {
     const crypto = new Crypto('Chill out.')
     expect(crypto.ciphertext).toEqual('clu hlt io ')
   })
 
-  test.skip('54 character plaintext results in 7 chunks, the last two with trailing spaces', () => {
+  test('54 character plaintext results in 7 chunks, the last two with trailing spaces', () => {
     const crypto = new Crypto(
       'If man was meant to stay on the ground, god would have given us roots.'
     )
--- typescript/crypto-square/crypto-square.ts
+++ typescript/crypto-square/crypto-square.ts
@@ -1,9 +1,37 @@
 export class Crypto {
+  private _plainText: unknown
   constructor(plainText: unknown) {
-    throw new Error('Remove this statement and implement this function')
+    this._plainText = plainText
   }
 
   get ciphertext(): unknown {
-    throw new Error('Remove this statement and implement this function')
+    let cipherText = ''
+    if (this._plainText && typeof this._plainText === "string") {
+      cipherText = this._plainText.replace(/[^a-zA-Z0-9+]/g, "").toLowerCase();
+      const columns = Math.ceil(Math.sqrt(cipherText.length))
+      let rows = Math.floor(Math.sqrt(cipherText.length))
+      if(rows * columns < cipherText.length) {
+        rows++
+      }
+      //add padding to the end of the text
+      if (cipherText.length < rows * columns) {
+        cipherText = cipherText.padEnd(rows*columns, ' ')
+      }
+
+      let normalisedTextMatrix = Array(rows)
+      for(let i=0; i<rows; i++) {
+        normalisedTextMatrix[i] = cipherText.substring(i*columns, (i+1)*columns)
+      }
+      let cipherTextMatrix: Array<string> = []
+      for(let i=0; i<columns; i++) {
+        let cipher = ''
+        for(let j=0; j<rows; j++) {
+          cipher += normalisedTextMatrix[j][i]
+        }
+        cipherTextMatrix[i] = cipher
+      }
+     cipherText = cipherTextMatrix.join(' ')
+    }
+    return cipherText
   }
 }
