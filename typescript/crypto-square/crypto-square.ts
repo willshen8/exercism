e4d97c994b1872e4c7191b3ce17e768d2671d28e
Will Shen <william.shen8@gmail.com>
2021-09-29 12:08:22 +1000

    refactor after review

--- typescript/crypto-square/crypto-square.ts
+++ typescript/crypto-square/crypto-square.ts
@@ -5,31 +5,26 @@ interface Dimension {
   columns: number
 }
 export class Crypto {
-  private _plainText: unknown
+  private _plainText: string
   private _cipherText: string = ''
-  constructor(plainText: unknown) {
+  constructor(plainText: string) {
     this._plainText = plainText
   }
 
-  get ciphertext(): unknown {
+  get ciphertext(): string {
     // lazy load encoded message - only calculate when requested
     if (!this._cipherText) {
-      let cipherText = ''
-      if (this._plainText && typeof this._plainText === "string") {
-        cipherText = this._plainText.replace(NonAlphanumericRegex, "").toLowerCase();
-
-        const { rows, columns } = this.calculateRectangleDimension(cipherText)
-        const normalisedTextArray = this.splitStringToArray(cipherText, rows, columns)
-
-        cipherText = this.encodeMessage(normalisedTextArray, rows, columns)
-      }
+      let cipherText = this._plainText.replace(NonAlphanumericRegex, "").toLowerCase();
+      const { rows, columns } = this.calculateRectangleDimension(cipherText)
+      const normalisedTextArray = this.splitStringToArray(cipherText, rows, columns)
+      cipherText = this.encodeMessage(normalisedTextArray, rows, columns)
       this._cipherText = cipherText
     }
     return this._cipherText
   }
 
   private encodeMessage(input: Array<string>, rows: number, columns: number): string {
-    let cipherTextMatrix: Array<string> = []
+    const cipherTextMatrix: Array<string> = []
     for (let i = 0; i < columns; i++) {
       let cipherRow = ''
       for (let j = 0; j < rows; j++) {
@@ -41,22 +36,24 @@ export class Crypto {
   }
 
   private calculateRectangleDimension(input: string): Dimension {
-    const columns = Math.ceil(Math.sqrt(input.length))
-    let rows = Math.floor(Math.sqrt(input.length))
+    const sqrRootOfInputLength = Math.sqrt(input.length)
+    const columns = Math.ceil(sqrRootOfInputLength)
+    let rows = Math.floor(sqrRootOfInputLength)
     if (rows * columns < input.length) {
       rows++
     }
-    return { rows: rows, columns: columns }
+    return { rows, columns }
   }
 
   private splitStringToArray(input: string, rows: number, columns: number): Array<string> {
+    let paddedInput = input.slice()
     if (input.length < rows * columns) {
-      input = input.padEnd(rows * columns, ' ')
+      paddedInput = paddedInput.padEnd(rows * columns, ' ')
     }
 
-    let outputArray = Array(rows)
+    const outputArray = Array(rows)
     for (let i = 0; i < rows; i++) {
-      outputArray[i] = input.substring(i * columns, (i + 1) * columns)
+      outputArray[i] = paddedInput.substring(i * columns, (i + 1) * columns)
     }
     return outputArray
   }
