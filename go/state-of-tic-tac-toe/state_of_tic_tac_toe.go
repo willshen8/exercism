cf32205301b251379b3676f6f40300501fd1f4db
willshen8 <william.shen8@gmail.com>
2022-10-25 20:49:21 +1100

    fix: refactor tictactoe

--- go/state-of-tic-tac-toe/state_of_tic_tac_toe.go
+++ go/state-of-tic-tac-toe/state_of_tic_tac_toe.go
@@ -14,28 +14,35 @@ const (
 )
 
 func StateOfTicTacToe(board []string) (State, error) {
-	statusBoard := strings.Split(strings.Join(board, ""), "")
-	if DetermineInvalidGame(statusBoard) {
+	statusBoard := []rune(strings.Join(board, ""))
+	hasXWon := isWinForPlayer('X', statusBoard)
+	hasOWon := isWinForPlayer('O', statusBoard)
+
+	if isInvalidGame(statusBoard) {
 		return "", fmt.Errorf("invalid game")
 	}
 
-	if DetermineWin("X", statusBoard) || DetermineWin("O", statusBoard) {
+	if hasXWon && hasOWon {
+		return "", fmt.Errorf("games has been played on after it was won")
+	}
+
+	if hasXWon || hasOWon {
 		return Win, nil
 	}
-	if DetermineDrawGame(statusBoard) {
+	if isDrawGame(statusBoard) {
 		return Draw, nil
 	}
 	return Ongoing, nil
 }
 
 // X starts, so number of X's should be equal to number of O's or 1 more
-func DetermineInvalidGame(statusBoard []string) bool {
+func isInvalidGame(statusBoard []rune) bool {
 	var xCount, oCount int
 	for _, v := range statusBoard {
-		if v == "X" {
+		if v == 'X' {
 			xCount++
 		}
-		if v == "O" {
+		if v == 'O' {
 			oCount++
 		}
 	}
@@ -43,27 +50,30 @@ func DetermineInvalidGame(statusBoard []string) bool {
 		return true
 	}
 	// determine if both wins
-	if DetermineWin("X", statusBoard) && DetermineWin("O", statusBoard) {
+	if isWinForPlayer('X', statusBoard) && isWinForPlayer('O', statusBoard) {
 		return true
 	}
 	return false
 }
 
-func DetermineDrawGame(statusBoard []string) bool {
+func isDrawGame(statusBoard []rune) bool {
 	var xCount, oCount int
 	for _, v := range statusBoard {
-		if v == "X" {
+		if v == 'X' {
 			xCount++
 		}
-		if v == "O" {
+		if v == 'O' {
 			oCount++
 		}
 	}
 	return xCount == 5 && oCount == 4
 }
 
-func DetermineWin(player string, board []string) bool {
-	// Horizontal
+func isWinForPlayer(player rune, board []rune) bool {
+	return isWinHorizontally(player, board) || isWinVertically(player, board) || IsWinDiagonally(player, board)
+}
+
+func isWinHorizontally(player rune, board []rune) bool {
 	if board[0] == player && board[1] == player && board[2] == player {
 		return true
 	}
@@ -73,8 +83,10 @@ func DetermineWin(player string, board []string) bool {
 	if board[6] == player && board[7] == player && board[8] == player {
 		return true
 	}
+	return false
+}
 
-	// Vertical
+func isWinVertically(player rune, board []rune) bool {
 	if board[0] == player && board[3] == player && board[6] == player {
 		return true
 	}
@@ -84,14 +96,15 @@ func DetermineWin(player string, board []string) bool {
 	if board[2] == player && board[5] == player && board[8] == player {
 		return true
 	}
+	return false
+}
 
-	// Diagonal
+func IsWinDiagonally(player rune, board []rune) bool {
 	if board[0] == player && board[4] == player && board[8] == player {
 		return true
 	}
 	if board[2] == player && board[4] == player && board[6] == player {
 		return true
 	}
-
 	return false
 }
