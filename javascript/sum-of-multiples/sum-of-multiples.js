8675d5706b117981d6cb51cb6b65a6d75b7a30e9
Will Shen <30467958+willshen8@users.noreply.github.com>
2025-11-09 16:35:14 +1100

    :sparkle: finish sum_of_mutliples

---
+++ javascript/sum-of-multiples/sum-of-multiples.js
@@ -0,0 +1,26 @@
+export const sum = (baseValues, levelCompleted) => {
+  if (levelCompleted <= 1 || baseValues.length === 0) return 0;
+
+  let uniqueMultiplesSet = new Set();
+  baseValues.forEach((baseValue) => {
+    if (baseValue === 0) return;
+
+    for (
+      let multiple = baseValue;
+      multiple < levelCompleted;
+      multiple += baseValue
+    ) {
+      uniqueMultiplesSet.add(multiple);
+    }
+  });
+
+  return sumOfSet(uniqueMultiplesSet);
+};
+
+const sumOfSet = (set) => {
+  let result = 0;
+  set.forEach((value) => {
+    result += value;
+  });
+  return result;
+};
